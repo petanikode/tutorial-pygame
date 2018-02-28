5581611352041c852add852ba78a7c72801cfea8
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2018-03-01 03:12:58 +0800

    add game background and castle object

--- game.py
+++ game.py
@@ -14,6 +14,8 @@ playerpos = [100, 100] # initial position for player
 # 3 - Load Game Assets ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3.1 - Load Images
 player = pygame.image.load("resources/images/dude.png")
+grass = pygame.image.load("resources/images/grass.png")
+castle = pygame.image.load("resources/images/castle.png")
 
 ## 4 - The Game Loop ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 while(running):
@@ -21,7 +23,18 @@ while(running):
     # 5 - Clear the screen ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     screen.fill(0)
     
-    # 6 - Draw the game object ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # 6 - Draw the game object ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # draw the grass
+    for x in range(int(width/grass.get_width()+1)):
+        for y in range(int(height/grass.get_height()+1)):
+            screen.blit(grass, (x*100, y*100))
+	
+	# draw the castle
+    screen.blit(castle, (0, 30))
+    screen.blit(castle, (0, 135))
+    screen.blit(castle, (0, 240))
+    screen.blit(castle, (0, 345))
+
     screen.blit(player, playerpos)
 
     # 7 - Update the sceeen ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
